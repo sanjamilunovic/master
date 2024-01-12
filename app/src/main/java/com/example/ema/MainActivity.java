a2269ab5d50648c1956d8a7b94bfc59b37d1222f
sanja1912 <sanja.milunovic@dev.telnet.rs>
2024-01-12 12:54:35 +0100

    Add FAB button on the UI

--- app/src/main/java/com/example/ema/MainActivity.java
+++ app/src/main/java/com/example/ema/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.ema;
 
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.google.android.material.appbar.MaterialToolbar;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.materialToolbar)
+    MaterialToolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
 
 
 
