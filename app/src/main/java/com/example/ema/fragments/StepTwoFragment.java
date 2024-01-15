068f42f2c21c9eef789d7454cd0b5d62d2c6242a
sanja1912 <sanja.milunovic@dev.telnet.rs>
2024-01-15 15:50:49 +0100

    Add vibration on full screen picture

--- app/src/main/java/com/example/ema/fragments/StepTwoFragment.java
+++ app/src/main/java/com/example/ema/fragments/StepTwoFragment.java
@@ -2,17 +2,21 @@ package com.example.ema.fragments;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.provider.MediaStore;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -22,6 +26,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.ema.R;
+import com.github.mmin18.widget.RealtimeBlurView;
 import com.stepstone.stepper.Step;
 import com.stepstone.stepper.StepperLayout;
 import com.stepstone.stepper.VerificationError;
@@ -43,6 +48,9 @@ public class StepTwoFragment extends Fragment implements Step,View.OnClickListen
     ImageView imageView;
     Dialog dialog;
     Bitmap bitmap;
+    private Vibrator vibrator;
+    private ScaleGestureDetector scaleGestureDetector;
+    private float scaleFactor = 1.0f;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.step_two_fragment, container, false);
@@ -50,10 +58,12 @@ public class StepTwoFragment extends Fragment implements Step,View.OnClickListen
 
         imageViewCamera.setOnClickListener(this);
         imageViewGallery.setOnClickListener(this);
+        vibrator = (Vibrator) getContext().getSystemService(Context.VIBRATOR_SERVICE);
 
         GestureDetector gestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
             @Override
             public void onLongPress(MotionEvent e) {
+                vibrate();
                 showFullScreenImage();
             }
         });
@@ -63,20 +73,34 @@ public class StepTwoFragment extends Fragment implements Step,View.OnClickListen
             public boolean onTouch(View v, MotionEvent event) {
                 gestureDetector.onTouchEvent(event);
 
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_UP:
-                        dialog.cancel();
-                        break;
-                }
                 return true;
             }
         });
+
         return v;
+    }
+
+
+
+
+    private void vibrate() {
+        // Vibrirajte 500 milisekundi
+        if (vibrator.hasVibrator()) {
+            vibrator.vibrate(100);
+        }
     }
 
     private void showFullScreenImage() {
         dialog = new Dialog(getContext(), android.R.style.Theme_Black_NoTitleBar_Fullscreen);
         dialog.setContentView(R.layout.dialog_full_screen_image);
+
+        RealtimeBlurView realtimeBlurView = dialog.findViewById(R.id.realtimeBlurView);
+        realtimeBlurView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
 
         ImageView fullScreenImageView = dialog.findViewById(R.id.fullScreenImageView);
         fullScreenImageView.setImageBitmap(bitmap);
