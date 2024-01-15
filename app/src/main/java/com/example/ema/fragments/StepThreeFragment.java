1f1ffeb0b5eadb4d4931812f476190834c00ac14
sanja1912 <sanja.milunovic@dev.telnet.rs>
2024-01-15 15:02:34 +0100

    Add speech to text feature

--- app/src/main/java/com/example/ema/fragments/StepThreeFragment.java
+++ app/src/main/java/com/example/ema/fragments/StepThreeFragment.java
@@ -1,6 +1,13 @@
 package com.example.ema.fragments;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +15,8 @@ import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import com.example.ema.R;
 import com.google.android.material.datepicker.MaterialDatePicker;
@@ -17,14 +26,23 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.stepstone.stepper.Step;
 import com.stepstone.stepper.VerificationError;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class StepThreeFragment extends Fragment implements Step, View.OnClickListener{
+    public static final Integer RecordAudioRequestCode = 1;
+    private SpeechRecognizer speechRecognizer;
     @BindView(R.id.tilPurchaseDate)
     TextInputLayout tilPurchaseDate;
+    @BindView(R.id.tilInvoice)
+    TextInputLayout tilInvoice;
     @BindView(R.id.etPurchaseDate)
     TextInputEditText etPurchaseDate;
+    @BindView(R.id.etInvoice)
+    TextInputEditText etInvoice;
     @BindView(R.id.spinnerCategory)
     MaterialAutoCompleteTextView spinnerCategory;
 
@@ -33,10 +51,81 @@ public class StepThreeFragment extends Fragment implements Step, View.OnClickLis
         View v = inflater.inflate(R.layout.fragment_step_three, container, false);
         ButterKnife.bind(this,v);
 
+        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED){
+            checkPermission();
+        }
+
+        speechRecognizer =  SpeechRecognizer.createSpeechRecognizer(getContext());
+
+        final Intent speechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                etInvoice.setText("");
+                etInvoice.setHint("Listening...");
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                ArrayList<String> data = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                etInvoice.setText(data.get(0));
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+        tilInvoice.setEndIconOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speechRecognizer.startListening(speechRecognizerIntent);
+            }
+        });
+
+
+
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(), R.array.categories, android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(R.layout.drop_down_item);
 
         spinnerCategory.setAdapter(adapter);
+
+
 
         tilPurchaseDate.setEndIconOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +136,12 @@ public class StepThreeFragment extends Fragment implements Step, View.OnClickLis
 
 
         return v;
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.RECORD_AUDIO},RecordAudioRequestCode);
+        }
     }
 
     private void showDatePicker() {
